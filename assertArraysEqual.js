3d8eb771a48ac791529ab652536ec872f74194bb
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-08 18:49:50 -0500

    Implemented eqArrays to assertArraysEqual for array comparison without using === check

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(array) {
+  if (array) {
+    return "Yes!";
+  }
+  else {
+    return "No!";
+  }
+};
+
+
+console.log(`Are the two array list equal? ${assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]))}`); // => true
+
+console.log(`Are the two array list equal? ${assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]))}`); // => false
+
+console.log(`Are the two array list equal? ${assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]))}`); // => true
+console.log(`Are the two array list equal? ${assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]))}`); // => false
+
+console.log(`Are the two array list equal? ${assertArraysEqual(eqArrays(["1", "2"], ["1", "2", "3"]))}`); // => false
+console.log(`Are the two array list equal? ${assertArraysEqual(eqArrays(["1", "2", "3"], ["2", "3"]))}`); // => false
