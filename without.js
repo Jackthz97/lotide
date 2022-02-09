bddcdd8aa5d7162c86e15a9f2287117f46940ea2
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-08 20:00:14 -0500

    Implemented without function to log a list of missing items array. Used assertArraysEqual function to make sure the original array was not altered by the without function.

---
+++ without.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let without = function(source, itemsToRemove) {
+  let missingItems = [];
+
+  for (let i = 0; i < source.length; i++) {
+    let found = false;
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        found = true;
+      }
+    }
+    if (found === false) {
+      missingItems.push(source[i]);
+    }
+  }
+  console.log(missingItems);
+};
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
