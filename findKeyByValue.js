b45758ee07e7511450ce1198d988431eae841453
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-09 20:36:21 -0500

    Implemented findKeyByValue function to search for a key on an object where its value matches a given value

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    // console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  } else {
+    // console.log("🚫🚫🚫Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (tvShow, showName) {
+  for (let show of Object.keys(tvShow)) {
+    if (tvShow[show] === showName){
+      return show;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Game of Thrones"), "drama");
