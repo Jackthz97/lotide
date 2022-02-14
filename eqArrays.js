2fcbec036ccc02dcf4bbbe0417e12b879a02d8ad
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-14 18:17:54 -0500

    refactored the eqArrays.js module to export its eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(actual, expected) {
   if (actual.length === expected.length) {
     for (let i = 0; i < actual.length; i++) {
@@ -18,10 +10,4 @@ const eqArrays = function(actual, expected) {
   return false;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
