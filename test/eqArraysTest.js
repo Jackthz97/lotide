3b527432bb3c15f74f9a10197e855746d03e04ea
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-14 18:23:00 -0500

    moved the test cases from eqArrays.js to eqArraysTest.js and required the assertEqual function to be used for the test cases

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,7 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
-
+//Test Cases
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
