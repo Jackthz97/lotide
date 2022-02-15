d5e1d5edf1479ee1c235e55a706888c369a1e24a
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-14 23:16:15 -0500

    module exported the countLetters function

--- countLetters.js
+++ countLetters.js
@@ -1,16 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(string) {
   const count = {};
-  for (let letter of string){
+  for (let letter of string) {
     if (letter !== " ") {
-      if (count[letter]){
+      if (count[letter]) {
         count[letter] += 1;
       } else {
         count[letter] = 1;
@@ -20,4 +12,4 @@ const countLetters = function(string) {
   return count;
 };
 
-console.log(countLetters("lighthouse in the house"));
+module.exports = countLetters;
