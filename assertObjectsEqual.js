260fdf696cdbbc6630832afbc90d15ec55274732
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-14 23:13:46 -0500

    module exported the assertObjectsEqual function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,36 +1,4 @@
-
-const eqArrays = function(actual, expected) {
-  if (actual.length === expected.length) {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-};
-
-const eqObjects = function(object1, object2) {
-  let keyList1 = Object.keys(object1);
-  let keyList2 = Object.keys(object2);
-  if (keyList1.length !== keyList2.length) {
-    return false;
-  }
-  for (let i = 0; i < keyList1.length; i++) {
-    if (!(keyList1[i] in object2)) {
-      return false;
-    }
-    if ((Array.isArray(object1[keyList1[i]]))) {
-      if (!(eqArrays(object1[keyList1[i]], object2[keyList1[i]]))) {
-        return false;
-      }
-    } else if (object1[keyList1[i]] !== object2[keyList1[i]]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -42,9 +10,6 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const cd = { c: "1", d: ["2", 3, 4] };
-assertObjectsEqual(ab, ba);
-assertObjectsEqual(ab, cd);
-assertObjectsEqual(ba, ab);
+module.exports = assertObjectsEqual;
+
+
